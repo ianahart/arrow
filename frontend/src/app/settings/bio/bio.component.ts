f6101926876e26f176dfc6a56711a452201b5470
Ian Hart <ianhart7790@gmail.com>
2022-11-20 12:55:29 -0500

    create profile bio files and bio textbox

---
+++ frontend/src/app/settings/bio/bio.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, OnInit, Output, Input} from '@angular/core';
+
+@Component({
+    selector: 'app-bio',
+    templateUrl: './bio.component.html',
+    styleUrls: ['./bio.component.css']
+})
+export class BioComponent implements OnInit {
+
+    @Input() bio = '';
+    @Output() bioTextEvent = new EventEmitter<string>();
+    charCount = 0;
+    constructor() {}
+
+    ngOnInit(): void {
+    }
+
+
+    onInputChange(value: string) {
+
+        this.bioTextEvent.emit(value);
+    }
+
+}
